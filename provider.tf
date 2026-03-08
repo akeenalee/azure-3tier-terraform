bd178aec22ab2442f2b756f0349825c8ee73a537
Alli Akintola <alliakintola@gmail.com>
2026-03-08 11:48:19 +0200

    feat: migrate Terraform state to Azure remote backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.90"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "allitfstate"
+    container_name       = "tfstate"
+    key                  = "3tier.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
